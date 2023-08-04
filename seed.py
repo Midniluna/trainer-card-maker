a9f4c9014de47623262ea34abeb1f76dbecef92e
root <midnightlunalux@gmail.com>
2023-08-04 19:26:44 -0400

    Modified seed file to make it way more efficient and include regional variants + gmax/mega forms

--- seed.py
+++ seed.py
@@ -17,37 +17,25 @@ def get_all_pkmn():
     r = data.json()
     r = r["results"]
     
+    # Now turn pokemon list into every pokemon, INCLUDING regional variants and megas/gmax formes. A bit time consuming to put all in database, BUT WAY less time consuming to reach into the database than to make an API request for every single pokemon. There are a lot of pokemon lol.
 
     for pokemon in r:
-
         single_poke = requests.get(pokemon["url"])
         req = single_poke.json()
 
-        # Get the dex number from the national pokedex
-        dexnum = req["pokedex_numbers"][0]["entry_number"]
-        # Get the url of the default form/regional variant
-        url = req["varieties"][0]["pokemon"]["url"]
-        new_mon = Pokemon(species_dexnum = dexnum, name = pokemon["name"], url = url)
-        db.session.add(new_mon)
-        db.session.commit()
-        print(f'pokemon: {new_mon.name} id: {new_mon.id}; ADDED')
+        for variant in req["varieties"]:
+            # Dex number will always be the same for same-species, regardless of variant
+            dexnum = req["pokedex_numbers"][0]["entry_number"]
 
-        # If the pokemon has multiple forms, add the other forms to the choices
-        if len(req["varieties"]) > 1:
-            for forme in req["varieties"]:
-                if forme["is_default"]:
-                    return
-                else:
-                    # dex number is the same since it's the same species. variant name and url are different.
-                    forme = Pokemon(species_dexnum = dexnum, name = forme["pokemon"]["name"], url = forme["pokemon"]["url"])
-                    db.session.add(forme)
-                    db.session.commit()
-                    print(f'pokemon: {forme.name} id: {forme.id}; ADDED')
-            return
+            # name and url changes depending on variant
+            name = variant["pokemon"]["name"]
+            url = variant["pokemon"]["url"]
 
-# Hang on.
-
-
-
+            # add pokemon to list
+            new_mon = Pokemon(species_dexnum = dexnum, name = name, url = url)
+            db.session.add(new_mon)
+            db.session.commit()
+            print(f'pokemon: {new_mon.name} id: {new_mon.id}; ADDED')
+            
 
 get_all_pkmn()
